402bc381cf943512d5ded34e7db97e8dc8a2761b
DilanAtomica <dilan-shwane@hotmail.com>
2022-06-27 11:33:31 +0200

    Prepared the starting files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {HashRouter, Route, Routes} from "react-router-dom";
+import {useState, createContext} from "react";
+
+export const AppContext = createContext();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <AppContext.Provider value={{}}>
+        <div className="App">
+          <HashRouter>
+            <Routes>
+              <Route path="/" element={} />
+            </Routes>
+          </HashRouter>
+        </div>
+      </AppContext.Provider>
   );
 }
 
